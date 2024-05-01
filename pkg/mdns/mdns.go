961d7df4f072eb3d5a0392fee5f6b392d3ef63b0
Gabriel Moura <gmoura96@icloud.com>
2024-05-01 01:51:29 -0300

    fix: fix mDNS

--- pkg/mdns/mdns.go
+++ pkg/mdns/mdns.go
@@ -3,8 +3,6 @@ package mdns
 import (
 	"net"
 	"os"
-	"os/exec"
-	"strings"
 
 	"github.com/hashicorp/mdns"
 )
@@ -30,52 +28,22 @@ func getLocalIP() []net.IP {
 // SetDNS configura o serviço de DNS multicast (mDNS).
 func SetDNS(appName string, appPort int) error {
 	// Obtém o nome do host
-	host, err := getHostname()
-	if err != nil {
-		return err
-	}
+	host, _ := os.Hostname()
 
 	// Cria as informações do serviço
 	info := []string{appName}
 
 	// Cria e configura o serviço mDNS
-	service, err := mdns.NewMDNSService(host, "_rpi._tcp", "", host, appPort, getLocalIP(), info)
+	service, err := mdns.NewMDNSService(host, "_rpi._tcp", "", "", appPort, getLocalIP(), info)
 	if err != nil {
 		return err
 	}
 
 	// Cria o servidor mDNS e defer sua parada
-	server, err := mdns.NewServer(&mdns.Config{Zone: service})
+	_, err = mdns.NewServer(&mdns.Config{Zone: service})
 	if err != nil {
 		return err
 	}
-	defer func() {
-		_ = server.Shutdown()
-	}()
 
 	return nil
-}
-
-func getHostname() (string, error) {
-	host, err := os.Hostname()
-	if err == nil {
-		// Adiciona um ponto ao final do nome do host para torná-lo um FQDN válido
-		return clean(host) + ".", nil
-	}
-
-	hostFile, err := exec.Command("cat", "/etc/hostname").Output()
-	if err != nil {
-		return "", err
-	}
-
-	// Adiciona um ponto ao final do nome do host lido do arquivo '/etc/hostname' para torná-lo um FQDN válido
-	return clean(string(hostFile)) + ".", nil
-}
-
-// clean removes specified substrings from the input string and trims the result.
-func clean(str string, args ...string) string {
-	for _, arg := range args {
-		str = strings.ReplaceAll(str, arg, "")
-	}
-	return strings.TrimSpace(str)
 }
